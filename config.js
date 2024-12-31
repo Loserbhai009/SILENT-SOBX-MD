71a4d690119ce22c8facb0527b145cf7da92f013
Loserbhai009 <g7975393@gmail.com>
2024-12-31 11:21:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7FQBmTYR#6_kGUzmo6w0BmXoFGKInQzhfWDeH9kVK2eT30MZqlQE",
+SESSION_ID: process.env.SESSION_ID || "SILENT-SOBX-MD~vJAkkJJZ#V7y7QESuhE8eEqJoZEub4llYuDuWBfq2mKt6Mp3Hjpk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/jc5o71.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M LOSER WHATSAPP BOT 😊♻️",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
